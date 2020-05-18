695b1108c95258bf64917cc971cd2b68e09de915
sashabaranov <677093+sashabaranov@users.noreply.github.com>
2020-05-18 14:50:57 +0300

    Remove unnecessary print

--- parser.go
+++ parser.go
@@ -130,7 +130,6 @@ func Parse(source io.Reader, validate bool) (*Element, error) {
 	if err != nil {
 		return nil, err
 	}
-	fmt.Printf("Node: %v\n", element.Name)
 	if err := element.Decode(decoder); err != nil && err != io.EOF {
 		return nil, err
 	}
